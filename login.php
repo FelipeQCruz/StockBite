aa56168403bb009d2f7f49df07ce3e148bd115aa
Felipe Q. Cruz <felipe.q.cruz@gmail.com>
2025-03-04 17:27:44 -0300

    Login e logout funcionando

--- login.php
+++ login.php
@@ -1,12 +1,7 @@
 <?php
 session_start(); // Deve ser a primeira linha
 
-// Dados para conexão com o MySQL
-$host = "localhost";
-$dbUsername = "root";
-$dbPassword = "27H09g94B*";
-$dbName = "stockbite";
-
+include "php/conexao.php";
 // Criando conexão
 $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
 
@@ -49,53 +44,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 $conn->close();
 ?>
-
-<!DOCTYPE html>
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login de usuário</title>
-    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
-</head>
-
-<body id="page-top">
-    <div id="wrapper">
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion">
-        </ul>
-        <div id="content-wrapper" class="d-flex flex-column">
-            <div id="content">
-                <div class="container-fluid">
-                    <h1 class="h3 mb-4 text-gray-800">Login de usuário</h1>
-                    <?php if (isset($erro)) {
-                        echo "<div class='alert alert-info'>$erro</div>";
-                    } ?>
-                    <form action="login.php" method="POST">
-                        <div class="form-group">
-                            <label for="email">E-mail</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="senha">senha</label>
-                            <input type="password" class="form-control" id="senha" name="senha" required>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Fazer login</button>
-
-                    </form>
-                </div>
-            </div>
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                </div>
-            </footer>
-        </div>
-    </div>
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="js/sb-admin-2.min.js"></script>
-</body>
-
-</html>
--- session_data.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-header('Content-Type: application/json');
-
-echo json_encode([
-    'nome' => $_SESSION['nome'] ?? 'Usuário',
-    'email' => $_SESSION['email'] ?? 'Não logado'
-]);
-?>
