181beb41df987ce1f53de02696617ed1b7e8e8ed
Felipe Q. Cruz <felipe.q.cruz@gmail.com>
2025-03-05 23:21:08 -0300

    Cadastro de fornecedor atualizado

--- php/cadastro_fornecedor.php
+++ php/cadastro_fornecedor.php
@@ -1,23 +1,8 @@
 <?php
 session_start();
 
-// Conexão com o banco de dados 
-$host = "localhost";
-$dbUsername = "root";
-$dbPassword = "27H09g94B*";
-$dbName = "stockbite";
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbName;charset=utf8", $dbUsername, $dbPassword);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erro ao conectar ao banco de dados: " . $e->getMessage());
-}
-
-$conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// Conexão com o banco de dados
+include "conexao.php";
 
 // Processamento do formulário
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -57,89 +42,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Produto</title>
-    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
-</head>
-
-<body id="page-top">
-    <div id="wrapper">
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion">
-            <a class="nav-link" href="index.php">
-                <i class="fas fa-fw fa-tachometer-alt"></i>
-                Dashboard
-            </a>
-            <a class="nav-link active" href="cadastro_fornecedor.php">
-                <i class="fas fa-box fa-sm fa-fw mr-2 text-gray-400"></i>
-                Cadastro de Produto
-            </a>
-        </ul>
-        <div id="content-wrapper" class="d-flex flex-column">
-            <div id="content">
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Bem-vindo, Usuário</span>
-                                <i class="fas fa-user-circle fa-2x text-gray-400"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <div class="container-fluid">
-                    <h1 class="h3 mb-4 text-gray-800">Cadastro de Fornecedor</h1>
-                    <?php if (isset($mensagem)) {
-                        echo "<div class='alert alert-info'>$mensagem</div>";
-                    } ?>
-                    <form action="cadastro_fornecedor.php" method="POST">
-                        <div class="form-group">
-                            <label for="empresa">Empresa</label>
-                            <input type="text" class="form-control" id="empresa" name="empresa" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="CNPJ">CNPJ</label>
-                            <input type="number" step="0.01" class="form-control" id="CNPJ" name="CNPJ" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="razao_social">Razão social</label>
-                            <input type="text" class="form-control" id="razao_social" name="razao_social" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="nome_vendedor">Nome vendedor</label>
-                            <input type="nome_vendedor" class="form-control" id="nome_vendedor" name="nome_vendedor" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="telefone">Telefone vendedor</label>
-                            <input type="text" class="form-control" id="telefone" name="telefone" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="email">E-mail vendedor</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Cadastrar Fornecedor</button>
-
-                    </form>
-                </div>
-            </div>
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
-                    </div>
-                </div>
-            </footer>
-        </div>
-    </div>
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="js/sb-admin-2.min.js"></script>
-</body>
-
-</html>
