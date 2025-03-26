7ed564e1f250f3aaf1cae31a003ccb23da2d2756
Henrique Grecchi <henrique.grecchi.hg@gmail.com>
2025-03-25 21:26:25 -0300

    Alteração de categoria e subcategoria

--- sql/cria_base_e_tabelas.sql
+++ sql/cria_base_e_tabelas.sql
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS categoria (
 );
 
 CREATE TABLE IF NOT EXISTS fornecedor (
-    ID INT AUTO_INCREMENT PRIMARY KEY AUTO_INCREMENT,
+    ID INT AUTO_INCREMENT PRIMARY KEY,
     empresa VARCHAR(255) NOT NULL,
     CNPJ BIGINT NOT NULL UNIQUE,
     razao_social VARCHAR(255) NOT NULL,
@@ -31,14 +31,14 @@ CREATE TABLE IF NOT EXISTS usuario (
 );
 
 CREATE TABLE IF NOT EXISTS restaurante (
-    id INT AUTO_INCREMENT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY ,
     cnpj BIGINT NOT NULL UNIQUE,
     nome VARCHAR(255) NOT NULL,
     razao_social VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS item (
-    ID INT AUTO_INCREMENT PRIMARY KEY AUTO_INCREMENT,
+    ID INT AUTO_INCREMENT PRIMARY KEY ,
     nome VARCHAR(255) NOT NULL,
     preco_unitario FLOAT NOT NULL,
     quantidade_medida INT,
@@ -50,7 +50,7 @@ CREATE TABLE IF NOT EXISTS item (
 );
 
 CREATE TABLE IF NOT EXISTS estoque (
-    ID INT AUTO_INCREMENT PRIMARY KEY AUTO_INCREMENT,
+    ID INT AUTO_INCREMENT PRIMARY KEY ,
     id_item INT NOT NULL,
     data_hora_entrada DATETIME NOT NULL,
     data_hora_saida DATETIME,
@@ -59,10 +59,9 @@ CREATE TABLE IF NOT EXISTS estoque (
 );
 
 CREATE TABLE IF NOT EXISTS validade (
-    ID_item AUTO_INCREMENT PRIMARY KEY AUTO_INCREMENT,
+    ID_item INT,
     validade DATE NOT NULL,
-    quantidade FLOAT NOT NULL,
-    PRIMARY KEY (ID_item, validade)
+    quantidade FLOAT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS usuarios_restaurantes (
@@ -90,7 +89,7 @@ SET
     NULL;
 
 ALTER TABLE
-    faruramento
+    faturamento
 ADD
     CONSTRAINT fk_restaurante_faturamento FOREIGN KEY (cnpj) REFERENCES restaurante(cnpj) ON DELETE
 SET
@@ -147,185 +146,112 @@ INSERT INTO
     categoria (nome, id, id_pai)
 VALUES
     ('Insumos', 1, NULL),
-    ('Temperos', 2, NULL),
-    ('Embalagens', 3, NULL),
-    ('Pao', 4, null),
-    ('Proteinas', 5, null),
-    ('Laticio', 6, null),
-    ('Hortifruti', 7, null),
-    ('Bebidas', 8, null),
-    ('Limpeza', 9, null),
-    ('Molhos', 10, null),
-    ('Descartaveis', 11, null);
+    ('Embalagens', 2, NULL),
+    ('Pao', 3, null),
+    ('Proteinas', 4, null),
+    ('Laticinio', 5, null),
+    ('Hortifruti', 6, null),
+    ('Bebidas', 7, null),
+    ('Limpeza', 8, null),
+    ('Molhos', 9, null),
+    ('Descartaveis', 10, null);
 
 -- Inserindo subcategorias para cada categoria principal
 INSERT INTO
     categoria (nome, id_pai)
 VALUES
     -- Subcategorias de Insumos
-    ('Extrato de tomate', 1),
-    ('Cerveja preta', 1),
-    ('Oleo de soja', 1),
-    ('Creme culinario', 1),
-    ('Leite integral', 1),
+    ('Tempero', 1),
     ('Farinha de trigo', 1),
+    ('Extrato de tomate', 1),
+    ('Oleo de soja', 1),
+    ('Oleo de algodão', 1),
+    ('Leite integral', 1),
     ('Farinha Panko', 1),
-    ('Açucar', 1),
-    ('Açucar mascavo', 1),
-    ('Molho ingles', 1),
-    ('Molho de pimenta', 1),
-    ('Molho shoyu', 1),
-    ('Mostarda Amarela', 1),
-    ('Mostarda escura', 1),
-    ('Ketchup', 1),
-    ('Barbecue', 1),
-    ('Azeite', 1),
-    ('Mel', 1),
-    ('Vinagre', 1),
-    ('Base americana', 1),
-    ('Proteina de soja', 1),
-    ('Arroz', 1),
-    ('Feijao carioca', 1),
-    ('Feijao preto', 1),
-    ('Azeitona verde', 1),
-    ('Alho frito', 1),
-    ('Alho descascado', 1),
-    ('Xarope de limao siciliano', 1),
-    ('Xarope de pink lemonade', 1),
-    ('Xarope de maça verde', 1),
-    ('Xarope de pessego', 1),
-    ('Base cha', 1),
-    ('Oreo', 1),
-    ('Pacoca', 1),
-    ('Nesqeuick', 1),
-    ('Waffle', 1),
-    ('Batata congelada', 1),
-    ('Nutella', 1),
-    -- Subcategorias de Temperos
-    ('Sal', 2),
-    ('Sal parrilha', 2),
-    ('Pimenta do reino preta', 2),
-    ('Paprica picante', 2),
-    ('Paprica doce', 2),
-    ('Alho em po', 2),
-    ('Creme de cebola', 2),
-    ('Caldo de bacon', 2),
-    ('Chimichurri', 2),
-    ('Lemon Pepper', 2),
-    ('Salsinha', 2),
-    ('Cebolinha', 2),
-    ('Cominho em po', 2),
-    ('Caldo de costela', 2),
+    ('Cerveja preta', 1),
+    ('Ketchup Galao', 1),
+    ('Barbecue Galao', 1),
+    ('Mostarda amarela', 1),
+    
     -- Subcategorias de Embalagens
-    ('Sacola Craft', 3),
-    ('Caixa de hamburguer/porcao', 3),
-    ('Papel Manteiga', 3),
+    ('Sacola Craft', 2),
+    ('Caixa de hamburguer/porcao', 2),
+    ('Papel Manteiga', 2),
+    
     -- Subcategorias de Pao
-    ('Pao Brioche', 4),
-    ('Pao Tigre', 4),
-    ('Pao Australiano', 4),
-    ('Pao kids', 4),
-    ('Pao hot dog', 4),
-    ('Pao mini', 4),
-    ('Pao pandora', 4),
+    ('Pao Brioche', 3),
+    ('Pao Tigre', 3),
+    ('Pao Australiano', 3),
+    ('Pao kids', 3),
+    ('Pao hot dog', 3),
+    ('Pao mini', 3),
+    ('Pao pandora', 3),
+    
     -- Subcategoria de proteinas
-    ('Carne', 5),
-    ('File de Frango', 5),
-    ('Linguiça toscana', 5),
-    ('Contra filet', 5),
-    ('Hamburguer veggie', 5),
-    ('Costela', 5),
-    ('Frango porcao', 5),
-    ('Bacon manta', 5),
-    ('Bacon fatiado', 5),
+    ('Carne', 4),
+    ('File de Frango', 4),
+    ('Linguiça toscana', 4),
+    ('Contra filet', 4),
+    ('Hamburguer veggie', 4),
+    ('Costela', 4),
+    ('Frango porcao', 4),
+    ('Bacon manta', 4),
+    ('Bacon fatiado', 4),
+    
     -- Subcategoria de laticinio
-    ('Queijo prato', 6),
-    ('Molho de cheddar', 6),
-    ('Bisnaga de cheddar', 6),
-    ('Bisnaga de requeijao', 6),
-    ('Queijo mussarela', 6),
-    ('Gorgonzola', 6),
+    ('Queijo prato', 5),
+    ('Molho de cheddar', 5),
+    ('Bisnaga de cheddar', 5),
+    ('Bisnaga de requeijao', 5),
+    ('Queijo mussarela', 5),
+    ('Gorgonzola', 5),
+    
     -- Subcategoria de Hortifruti
-    ('Alface', 7),
-    ('Tomate', 7),
-    ('Cebola', 7),
-    ('Cebola roxa', 7),
-    ('Rucula', 7),
-    ('Laranja', 7),
-    ('Limao', 7),
-    ('Maracuja', 7),
-    ('Kiwi', 7),
-    ('Morango', 7),
-    ('Batata', 7),
+	('Hortifruti',6),
+    
     -- Subcategoria Bebidas
-    ('Agua sem gas 500ml', 8),
-    ('Agua com gas 500ml', 8),
-    ('Coca cola ks 290ml', 8),
-    ('Coca cola lata 350ml', 8),
-    ('Coca cola lata zero 350ml', 8),
-    ('Coca cola 2 litros', 8),
-    ('Coca cola zero 2 litros', 8),
-    ('Guarana lata 350ml', 8),
-    ('Guarana zero lata 350ml', 8),
-    ('Guarana 2 litros', 8),
-    ('Fanta laranja lata 350ml', 8),
-    ('Sprite lata 350ml', 8),
-    ('Schewppes citrus lata 350ml', 8),
-    ('Schewppes tonica lata 350ml', 8),
-    ('Kapo uva', 8),
-    ('Kapo laranja', 8),
-    ('Kapo Morango', 8),
-    ('H2oh limoneto 500ml', 8),
-    ('H2oh limao 500ml', 8),
-    ('Corona Long neck 330ml', 8),
-    ('Corona retornavel 600ml', 8),
-    ('Heineken long neck 330ml', 8),
-    ('Heineken zero long neck 330ml', 8),
-    ('Heineken lata 350ml', 8),
-    ('Heineken retornavel 600ml', 8),
-    ('Baden golden 600ml', 8),
-    ('Baden peach 600ml', 8),
-    ('Baden witbier 600ml', 8),
-    ('Colorado ribeirao', 8),
-    ('Goose ipa Long neck 330ml', 8),
-    ('Goose midway long neck 330ml', 8),
+	('Refrigerante', 7),
+    ('Cerveja', 7),
+    ('Destilado',7),
+    ('Não alcolico', 7),
+    
     -- Subcategoria Limpeza
-    ('Saco de lixo', 9),
-    ('Perflex', 9),
-    ('Detergente', 9),
-    ('Papel Toalha', 9),
-    ('Bobina de saco picotado', 9),
-    ('Luva de vinil', 9),
-    ('Agua sanitaria', 9),
-    ('Desinfetante', 9),
-    ('Azulim', 9),
-    ('Limpa vidro', 9),
-    ('Fibraço', 9),
-    ('Flotex', 9),
-    ('G-food', 9),
-    ('Peroxi food', 9),
-    ('Bucha de louça', 9),
-    ('Sanitizante', 9),
+    ('Saco de lixo', 8),
+    ('Perfex', 8),
+    ('Detergente', 8),
+    ('Papel Toalha', 8),
+    ('Bobina de saco picotado', 8),
+    ('Luva de vinil', 8),
+    ('Agua sanitaria', 8),
+    ('Desinfetante', 8),
+    ('Azulim', 8),
+    ('Limpa vidro', 8),
+    ('Fibraço', 8),
+    ('Flotex', 8),
+    ('G-food', 8),
+    ('Peroxi food', 8),
+    ('Bucha de louça', 8),
+    ('Sanitizante', 8),
     -- Subcategoria molhos
-    ('Maionese verde', 10),
-    ('Maionese de alho', 10),
-    ('Maionese de bacon', 10),
-    ('Maionese de pimenta', 10),
-    ('Molho de queijo', 10),
-    ('Molho de gorgonzola', 10),
-    ('Molho de ervas', 10),
+    ('Maionese verde', 9),
+    ('Maionese de alho', 9),
+    ('Maionese de bacon', 9),
+    ('Maionese de pimenta', 9),
+    ('Molho de queijo', 9),
+    ('Molho de gorgonzola', 9),
+    ('Molho de ervas', 9),
     -- Subcategoria descartaveis
-    ('Copo de 300 ml', 11),
-    ('Copo de 400 ml', 11),
-    ('Copo de 500 ml', 11),
-    ('Marmita de 250 ml', 11),
-    ('Marmita de 500 ml', 11),
-    ('Marmita de 750 ml', 11),
-    ('Bobina termica', 11),
-    ('Separador de hamburguer', 11),
-    ('Pá de maionese', 11),
-    ('Palito de hamburguer', 11),
-    ('Palito de porcao', 11);
+    ('Copo de 300 ml', 10),
+    ('Copo de 400 ml', 10),
+    ('Copo de 500 ml', 10),
+    ('Marmita de 250 ml', 10),
+    ('Marmita de 500 ml', 10),
+    ('Marmita de 750 ml', 10),
+    ('Bobina termica', 10),
+    ('Separador de hamburguer', 10),
+    ('Pá de maionese', 10),
+    ('Palito de hamburguer', 10),
+    ('Palito de porcao', 10);
 
 INSERT INTO
     perfis (perfis)
@@ -345,4 +271,4 @@ VALUES
 INSERT INTO
     restaurante (cnpj, nome, razao_social)
 VALUES
-    (43447633867, 'Felipe QC', 'FQC');
+    (26064269000125, 'Atlantis Burguer', 'Atlantis Burguer Hamburgueria LTDA');
